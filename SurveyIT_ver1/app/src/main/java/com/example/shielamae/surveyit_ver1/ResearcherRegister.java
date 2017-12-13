e537d0ff81ac9c56ec62b095cc2c4a74b03f6ab7
smav06 <34511401+smav06@users.noreply.github.com>
2017-12-13 21:27:13 +0800

    Reformat Register activity

--- SurveyIT_ver1/app/src/main/java/com/example/shielamae/surveyit_ver1/ResearcherRegister.java
+++ SurveyIT_ver1/app/src/main/java/com/example/shielamae/surveyit_ver1/ResearcherRegister.java
@@ -30,37 +30,35 @@ public class ResearcherRegister extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(etName.getText().toString().equals("")||etUsername.getText().toString().equals("")||etPassword.getText().toString().equals("")||etVPassword.getText().toString().equals("")){
+                if (etName.getText().toString().equals("") || etUsername.getText().toString().equals("") || etPassword.getText().toString().equals("") || etVPassword.getText().toString().equals("")) {
                     builder = new AlertDialog.Builder(ResearcherRegister.this);
                     builder.setTitle("Something went wrong...");
                     builder.setMessage("Please fill all the fields provided");
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int which){
+                        public void onClick(DialogInterface dialog, int which) {
                             dialog.dismiss();
                         }
                     });
-                    AlertDialog alertDialog =  builder.create();
+                    AlertDialog alertDialog = builder.create();
                     alertDialog.show();
-                }
-                else if (!(etPassword.getText().toString().equals(etVPassword.getText().toString()))){
+                } else if (!(etPassword.getText().toString().equals(etVPassword.getText().toString()))) {
                     builder = new AlertDialog.Builder(ResearcherRegister.this);
                     builder.setTitle("Your password does not match");
                     builder.setMessage("Match your password");
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int which){
+                        public void onClick(DialogInterface dialog, int which) {
                             dialog.dismiss();
                             etPassword.setText("");
                             etVPassword.setText("");
                         }
                     });
-                    AlertDialog alertDialog =  builder.create();
+                    AlertDialog alertDialog = builder.create();
                     alertDialog.show();
-                }
-                else {
-                   LoginRegisterTask lrTask = new LoginRegisterTask(ResearcherRegister.this);
-                   lrTask.execute("register", etName.getText().toString(),etUsername.getText().toString(),etPassword.getText().toString());
+                } else {
+                    LoginRegisterTask lrTask = new LoginRegisterTask(ResearcherRegister.this);
+                    lrTask.execute("register", etName.getText().toString(), etUsername.getText().toString(), etPassword.getText().toString());
                 }
             }
         });
