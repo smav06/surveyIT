dff6afb24ed9737127fb864ba539b7985c40fcaa
Hublot Hindot <hindot@github.com>
2017-12-20 02:09:23 +0800

    Add new activities and layouts

---
+++ SurveyIT_ver1/app/src/main/java/com/example/shielamae/surveyit_ver1/ResearcherViewSurvey.java
@@ -0,0 +1,20 @@
+package com.example.shielamae.surveyit_ver1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by jcgut on 12/20/2017.
+ */
+
+public class ResearcherViewSurvey extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_researcher_viewsurvey);
+
+
+    }
+}
