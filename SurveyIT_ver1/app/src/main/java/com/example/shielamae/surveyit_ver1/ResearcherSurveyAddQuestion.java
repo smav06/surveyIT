35642198d26f6d817590afc72f2932279b77725a
smav06 <34511401+smav06@users.noreply.github.com>
2017-12-20 20:37:29 +0800

    Added icon on src

---
+++ SurveyIT_ver1/app/src/main/java/com/example/shielamae/surveyit_ver1/ResearcherSurveyAddQuestion.java
@@ -0,0 +1,15 @@
+package com.example.shielamae.surveyit_ver1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Shiela Mae on 20/12/2017.
+ */
+
+public class ResearcherSurveyAddQuestion extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){}
+        super.onCreate(savedInstanceState);
+}
